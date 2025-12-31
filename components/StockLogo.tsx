a45a6c35ef79416e65e118bebe0305ed8e9809e9
Itamar Besor <itamarbesor@Itamars-MacBook-Air.local>
2025-12-31 14:31:07 +0200

    Add cache-busting to transparent PNG logos

--- components/StockLogo.tsx
+++ components/StockLogo.tsx
@@ -46,7 +46,7 @@ export default function StockLogo({ symbol, size = 'md' }: StockLogoProps) {
       }}
     >
       <img
-        src={logo.src}
+        src={`${logo.src}?v=transparent`}
         alt={logo.alt}
         className="w-full h-full object-contain"
         style={{
